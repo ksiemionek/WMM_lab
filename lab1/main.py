d567891f6c470b8a51378185911d684c1f81a116
ksiemion <01187279@pw.edu.pl>
2025-03-13 21:09:39 +0100

    defined main file with empty functions

---
+++ lab1/main.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def ex1():
+    pass
+
+
+def ex2():
+    pass
+
+
+def ex3():
+    pass
+
+
+def ex4():
+    pass
+
+
+if __name__ == "__main__":
+    # ex1()
+    # ex2()
+    # ex3()
+    ex4()
