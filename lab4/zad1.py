93620908a47c6dbc5047da23d7ce2bf92d82669d
ksiemion <01187279@pw.edu.pl>
2025-03-23 01:46:33 +0100

    fixed zad1

--- lab4/zad1.py
+++ lab4/zad1.py
@@ -2,7 +2,6 @@ import cv2
 from matplotlib import pyplot as plt
 from functions import calcPSNR
 
-
 masks = [(3, 3), (5, 5), (7, 7)]
 
 img_original = cv2.imread('./images/girl_col.png', cv2.IMREAD_UNCHANGED)
@@ -12,7 +11,7 @@ img_inoise = cv2.imread('./images/girl_col_inoise.png', cv2.IMREAD_UNCHANGED)
 psnr_noise = calcPSNR(img_original, img_noise)
 psnr_inoise = calcPSNR(img_original, img_inoise)
 
-fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+fig, axes = plt.subplots(1, 3, figsize=(15, 4))
 
 axes[0].imshow(cv2.cvtColor(img_original, cv2.COLOR_BGR2RGB))
 axes[0].set_title("Oryginał")
@@ -37,17 +36,36 @@ for mask in masks:
     img_median_blur = cv2.medianBlur(img_inoise, mask[0])
     psnr_median = calcPSNR(img_original, img_median_blur)
 
-    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+    fig, axes = plt.subplots(1, 2, figsize=(10, 4))
 
     axes[0].imshow(cv2.cvtColor(img_gaussian_blur, cv2.COLOR_BGR2RGB))
-    axes[0].set_title(f"Gauss {mask} (PSNR: {psnr_gauss:.2f})")
+    axes[0].set_title(f"Filtr Gaussa {mask} (PSNR: {psnr_gauss:.2f})")
     axes[0].axis('off')
 
     axes[1].imshow(cv2.cvtColor(img_median_blur, cv2.COLOR_BGR2RGB))
-    axes[1].set_title(f"Median {mask} (PSNR: {psnr_median:.2f})")
+    axes[1].set_title(f"Filtr medianowy {mask} (PSNR: {psnr_median:.2f})")
     axes[1].axis('off')
 
     plt.tight_layout()
-    filename = f"./images/zad1_maska_{mask[0]}x{mask[1]}.png"
-    plt.savefig(filename)
+    plt.savefig(f"./images/zad1_noise_maska_{mask[0]}x{mask[1]}.png")
+    plt.close()
+
+    img_gaussian_blur_inoise = cv2.GaussianBlur(img_inoise, mask, 0)
+    psnr_gauss_inoise = calcPSNR(img_original, img_gaussian_blur_inoise)
+
+    img_median_blur_inoise = cv2.medianBlur(img_inoise, mask[0])
+    psnr_median_inoise = calcPSNR(img_original, img_median_blur_inoise)
+
+    fig, axes = plt.subplots(1, 2, figsize=(10, 4))
+
+    axes[0].imshow(cv2.cvtColor(img_gaussian_blur_inoise, cv2.COLOR_BGR2RGB))
+    axes[0].set_title(f"Filtr Gaussa {mask} (PSNR: {psnr_gauss_inoise:.2f})")
+    axes[0].axis('off')
+
+    axes[1].imshow(cv2.cvtColor(img_median_blur_inoise, cv2.COLOR_BGR2RGB))
+    axes[1].set_title(f"Filtr medianowy {mask} (PSNR: {psnr_median_inoise:.2f})")
+    axes[1].axis('off')
+
+    plt.tight_layout()
+    plt.savefig(f"./images/zad1_inoise_maska_{mask[0]}x{mask[1]}.png")
     plt.close()
